97de1574a4718490be63bdaabd9f27f8a0082e0c
Mosu25 <mosu.avms@gmail.com>
2023-08-08 21:58:36 +0530

    adding for loop

---
+++ forLoop.js
@@ -0,0 +1,27 @@
+// Loops -> It's offer a quick and easy way to do something repeatedly
+/*  multiline commets shift + alt + a, single line comment -> cntl + /
+1. for
+2. do ... while
+3. while
+4. labeled
+5. break
+6. continue
+7. for... in
+8. for .. of 
+ */
+
+// line of code down and up moving using alt + up and alt + down 
+
+for (let i=0; i<5 ; i++) {
+    
+    console.log("Welcome mosu");
+    
+}
+
+
+// tables 
+
+
+for(let i=1; i<=10; i++){
+console.log(2*i);
+}
