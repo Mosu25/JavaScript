8756eb88fd00630317826cb9e519ab2e0fe980fe
Mosu25 <mosu.avms@gmail.com>
2023-08-08 21:59:39 +0530

    adding while and do-while

---
+++ whileAndDoWhile.js
@@ -0,0 +1,20 @@
+let i = 0;
+let max = 5;
+
+while (i < max) {
+    
+    console.log("Value of I: " + i);
+    i++;
+
+}
+
+max = -5;
+
+// do while
+
+do {
+    
+    console.log(i);
+    i++;
+
+} while(i < max);
