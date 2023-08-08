bb796154a731036de975df155819239699b0ab6e
Mosu25 <mosu.avms@gmail.com>
2023-08-08 21:41:55 +0530

    initial commit

---
+++ dataType.js
@@ -0,0 +1,22 @@
+//let, const, var
+
+
+//primitive -> boolean, number, bigint string & symbol
+//composite -> Object, arrays & functions
+//special -> undefined, null
+
+let age=34;
+console.log(typeof age)
+
+age="Welcome Mosu"
+console.log(age)
+console.log(typeof age)
+
+let a =10
+let b="20"
+console.log(a+b)  // it's dynamic language so this problem will happen. This issue resolved in TypeSciprt
+
+let isJavaScriptGood = true
+console.log(typeof isJavaScriptGood)
+isJavaScriptGood = "Yes Good"
+console.log(typeof isJavaScriptGood)
