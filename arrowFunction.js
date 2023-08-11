46847c0ffef98398c08ce3a3aadc8716a07b889d
Mosu25 <mosu.avms@gmail.com>
2023-08-11 23:56:45 +0530

    adding arrow functions

---
+++ arrowFunction.js
@@ -0,0 +1,10 @@
+const square = num => num * num ;
+const result = square(4);
+console.log(result)
+
+const msg = () => "Hello JS";
+console.log(msg());
+
+const add = (a,b) => a+b;
+console.log(add(4,6));
+
